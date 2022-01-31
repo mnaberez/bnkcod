6824d4346ebc849adc0b3f4911a4438be84e21c5
Mike Naberezny <mike@naberezny.com>
2022-01-30 17:53:55 -0800

    Add notes on possible usage

--- bnkcod.asm
+++ bnkcod.asm
@@ -4,9 +4,9 @@
     index = 0x1f            ;Pointer: Utility (various uses)
     cinv = 0x90             ;BASIC 2-4 CINV interrupt vector
     cbinv = 0x92            ;BASIC 2-4 BRK instruction interrupt vector
-    bufpnt1 = 0xbb          ;Pointer: Tape I/O Buffer #1
-    bufpnt2 = 0xbc          ;Pointer: Tape I/O Buffer #1
-    bitci = 0xbe            ;Cassette temp
+    bufpnt1 = 0xbb          ;Pointer: Tape I/O Buffer #1 \
+    bufpnt2 = 0xbc          ;Pointer: Tape I/O Buffer #1 | Cleared but not obviously used
+    bitci = 0xbe            ;Cassette temp               /
     mem_00ff = 0x00ff       ;Unused
     stack = 0x0100          ;Bottom of the stack
     mem_03fa = 0x03fa       ;Usused
@@ -20,26 +20,40 @@
     mem_f92b = 0xf92b       ;KERNAL
     mem_fcc0 = 0xfcc0       ;KERNAL
 
+;SuperPET RAM Usage
+;  9000-9001 Contains an address that will be jumped to on IRQ (lab_a000)
+;  9002 Bank number that the code pointed to by 9000 is in
+;  9003 Bank number that the code pointed to by 9004 is in
+;  9004-9005 Contains an address that will be jumped to on BRK (sub_a066)
+;
+;This ROM
+;  A000 Indirect jump to [9000]
+;  A003 Install: puts our routines in CINV and CBINV (SYS 49060)
+;  A01E Uninstall: puts the normal CINV and CBINV back (SYS 40990)
+
 lab_a000:
     jmp [expram]            ;a000  6c 00 90
 
 lab_a003:
     sei                     ;a003  78
+
+    ;Install our routine at A034 into CINV
     ldx #<lab_a034_cinv     ;a004  a2 34
     stx cinv                ;a006  86 90
     ldx #>lab_a034_cinv     ;a008  a2 a0
     stx cinv+1              ;a00a  86 91
 
+    ;Install our routine at A069 into CBINV
     ldx #<lab_a069_cbinv    ;a00c  a2 69
     stx cbinv               ;a00e  86 92
     ldx #>lab_a069_cbinv    ;a010  a2 a0
     stx cbinv+1             ;a012  86 93
     cli                     ;a014  58
 
-    ldx #0x00               ;a015  a2 00
-    stx bitci               ;a017  86 be
-    stx bufpnt1             ;a019  86 bb
-    stx bufpnt2             ;a01b  86 bc
+    ldx #0x00               ;a015  a2 00      \
+    stx bitci               ;a017  86 be      |   Not obviously used
+    stx bufpnt1             ;a019  86 bb      |   elsewhere in this code
+    stx bufpnt2             ;a01b  86 bc      /
     rts                     ;a01d  60
 
 lab_a01e:
@@ -57,13 +71,16 @@ lab_a01e:
 
     rts                     ;a033  60
 
+;CINV enters here
 lab_a034_cinv:
     lda stack+5,x           ;a034  bd 05 01
+
     cmp #<mem_f92b          ;a037  c9 2b
     bne lab_a045            ;a039  d0 0a
     lda stack+6,x           ;a03b  bd 06 01
     cmp #>mem_f92b          ;a03e  c9 f9
     bne lab_a045            ;a040  d0 03
+
     jsr mem_fcc0            ;a042  20 c0 fc
 
 lab_a045:
@@ -75,7 +92,7 @@ lab_a045:
     pha                     ;a04c  48
     pha                     ;a04d  48
     pha                     ;a04e  48
-    jmp mem_e455            ;a04f  4c 55 e4
+    jmp mem_e455            ;a04f  4c 55 e4     Jump into the middle of main IRQ routine
 
 lab_a052:
     lda expram+2            ;a052  ad 02 90
@@ -85,11 +102,12 @@ lab_a052:
     jsr sub_a066            ;a05c  20 66 a0
     pla                     ;a05f  68
     sta banksel             ;a060  8d fc ef
-    jmp mem_e600            ;a063  4c 00 e6
+    jmp mem_e600            ;a063  4c 00 e6     Exit from IRQ via normal exit
 
 sub_a066:
     jmp [expram+4]          ;a066  6c 04 90
 
+;CBINV enters here
 lab_a069_cbinv:
     lda index               ;a069  a5 1f
     pha                     ;a06b  48
@@ -134,7 +152,7 @@ lab_a091:
     txs                     ;a0a2  9a
     pla                     ;a0a3  68
     sta banksel             ;a0a4  8d fc ef
-    jmp mem_e600            ;a0a7  4c 00 e6
+    jmp mem_e600            ;a0a7  4c 00 e6     Exit from IRQ via normal exit
 
 lab_a0aa:
     lda stack+7,x           ;a0aa  bd 07 01
@@ -150,4 +168,4 @@ lab_a0b0:
     pla                     ;a0ba  68
     sta banksel             ;a0bb  8d fc ef
     pla                     ;a0be  68
-    jmp mem_e600            ;a0bf  4c 00 e6
+    jmp mem_e600            ;a0bf  4c 00 e6     Exit from IRQ via normal exit
